5ab6fed3c870f41004273a65ae4676aa064ca838
Boloo66 <99683557+Boloo66@users.noreply.github.com>
2022-05-18 15:09:45 +0100

    Update aux_error1.c

--- aux_error1.c
+++ aux_error1.c
@@ -4,10 +4,10 @@
  * strcat_cd - function that concatenates the message for cd error
  *
  * @datash: data relevant (directory)
- * @msg: message to print
- * @error: output message
- * @ver_str: counter lines
- * Return: error message
+ * @msg: message to print to stdin
+ * @error: message to the output program
+ * @ver_str: count the lines
+ * Return: saved error message
  */
 char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
 {
